f81a03bfcf4ad46c38190fe4dce82fc17cc41a1a
Marsel Gabbasov <marsel13_98@mail.ru>
2022-08-04 17:03:44 +0500

    fix modals

--- src/js/components/product-card.js
+++ src/js/components/product-card.js
@@ -6,11 +6,11 @@ $(".product-card__cart").click(function () {
     $(this).addClass("product-card__cart--active")
 })
 
-$(".product-card .ccalc-minus").click(function () {
-    let val = $(this).siblings("input").val()
-    console.log(val)
-    if(+val === 1){
-        $(this).parents(".product-card").find(".product-card__cart").removeClass("product-card__cart--active")
-    }
-})
+// $(".product-card .ccalc-minus").click(function () {
+//     let val = $(this).siblings("input").val()
+//     console.log(val)
+//     if(+val === 1){
+//         $(this).parents(".product-card").find(".product-card__cart").removeClass("product-card__cart--active")
+//     }
+// })
 
